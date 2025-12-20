389caa254fbb081550e93e9f60c7d1238b16e3fd
foundingrain <foundingrain@pm.me>
2025-12-20 01:05:39 -0500

    ssh autostart conflict w/ gnome (disabled)

--- configuration.nix
+++ configuration.nix
@@ -146,7 +146,7 @@
     #  wget
   ];
 
-  programs.ssh.startAgent = true;
+  # programs.ssh.startAgent = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
