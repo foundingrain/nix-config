3f19cc13ba29ef147379e80acf2c69fe1f191ce0
foundingrain <foundingrain@pm.me>
2026-01-06 19:04:26 -0500

    enableRedistributableFirmware set to true (for wifi issues)

--- hosts/mashnix/configuration.nix
+++ hosts/mashnix/configuration.nix
@@ -66,6 +66,7 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+  hardware.enableRedistributableFirmware = true;
 
   # Set your time zone.
   time.timeZone = "America/New_York";
