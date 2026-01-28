47b41c7df71045ac78bbf51f223a28d251f3ad5a
foundingrain <foundingrain@pm.me>
2026-01-28 13:00:24 -0500

    swapped thonknix to plasma

--- hosts/thonknix/configuration.nix
+++ hosts/thonknix/configuration.nix
@@ -31,10 +31,10 @@
 
     # Desktop Environments
     # ../../modules/cosmic.nix
-    ../../modules/gnome.nix
+    # ../../modules/gnome.nix
     # ../../modules/hyprland.nix
     ../../modules/niri.nix
-    # ../../modules/plasma.nix
+    ../../modules/plasma.nix
 
     # Graphics
     #  ../../modules/nvidia.nix
