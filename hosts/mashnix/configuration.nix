7aeaca677ef32f699820a4d498706c736edac4e3
foundingrain <foundingrain@pm.me>
2026-01-30 12:08:24 -0500

    enabled plasma as a sane default

--- hosts/mashnix/configuration.nix
+++ hosts/mashnix/configuration.nix
@@ -29,11 +29,11 @@
     # ../../modules/gnome-rdp.nix
 
     # Desktop Environments
-    ../../modules/cosmic.nix
+    # ../../modules/cosmic.nix
     # ../../modules/gnome.nix
     # ../../modules/hyprland.nix
     ../../modules/niri.nix
-    # ../../modules/plasma.nix
+    ../../modules/plasma.nix
 
     # Graphics
     #  ../../modules/nvidia.nix
