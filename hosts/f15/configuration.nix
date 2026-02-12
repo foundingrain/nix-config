ceb6f9488b06c25a97117fb0481ed1fa8bf449b4
foundingrain <foundingrain@pm.me>
2026-02-12 06:02:59 -0500

    swapped f15 to gnome

--- hosts/f15/configuration.nix
+++ hosts/f15/configuration.nix
@@ -11,12 +11,12 @@
 
     # ./modules/ai.nix
     ../../modules/corepackages.nix
-    ../../modules/cosmic.nix
+    # ../../modules/cosmic.nix
     ../../modules/development.nix
     # ../../modules/weylus.nix
     ../../modules/extra.nix
     ../../modules/gaming.nix
-    # ../../modules/gnome.nix
+    ../../modules/gnome.nix
     ../../modules/niri.nix
     # ../../modules/plasma.nix
     # ../../modules/hyprland.nix
