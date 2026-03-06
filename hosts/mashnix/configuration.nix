6fc68d1f15c9d07823e65a126e36fcb3cecb2157
foundingrain <foundingrain@pm.me>
2026-03-06 03:19:07 -0500

    changed to gnome cause curious

--- hosts/mashnix/configuration.nix
+++ hosts/mashnix/configuration.nix
@@ -30,9 +30,9 @@
 
     # Desktop Environments
     # ../../modules/cosmic.nix
-    # ../../modules/gnome.nix
+    ../../modules/gnome.nix
     # ../../modules/hyprland.nix
-    ../../modules/niri.nix
+    # ../../modules/niri.nix
     ../../modules/plasma.nix
 
     # Graphics
