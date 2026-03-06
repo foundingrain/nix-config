693e1608a7b8bdf109aae8da2b12f2a5b898dbf8
foundingrain <foundingrain@pm.me>
2026-03-06 03:42:26 -0500

    commented out virt-manager for update failure

--- modules/vm.nix
+++ modules/vm.nix
@@ -2,23 +2,8 @@
 
 {
   # Virt-Manager
-  programs.virt-manager.enable = true;
-  users.groups.libvertd.members = [ "neo" ];
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
-
-  # VMWare
- # virtualisation.vmware.host.enable = true;
-
-  #Quickemu
- # environment.systemPackages = with pkgs; [ quickemu ];
-  
-  # VirtualBox
-/*  users.extraGroups.vboxusers.members = [ "neo" ];
- # nixpkgs.config.allowUnfree = true;
-  virtualisation.virtualbox.host = {
-    enable = true;
-    # Unfree Oracle Extensions
-    enableExtensionPack = true;
-  }; */
+  # programs.virt-manager.enable = true;
+  # users.groups.libvertd.members = [ "neo" ];
+  # virtualisation.libvirtd.enable = true;
+  # virtualisation.spiceUSBRedirection.enable = true;
 }
