6b718473625e749b33acd6ce98c97617e9c12512
neo <foundingrain@pm.me>
2026-03-11 23:51:32 -0400

    added vbox host

--- flake.nix
+++ flake.nix
@@ -89,6 +89,7 @@
         mashnix = mkHost { hostName = "mashnix"; };
         f15 = mkHost { hostName = "f15"; };
         thonknix = mkHost { hostName = "thonknix"; };
+	vbox = mkHost { hostName = "vbox"; };
       };
     };
 }
