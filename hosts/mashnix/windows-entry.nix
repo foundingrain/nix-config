ab062bd7167989af85c91d8d5270486fa5093973
foundingrain <foundingrain@pm.me>
2026-03-14 18:51:38 -0400

    longer limine timeout

--- hosts/mashnix/windows-entry.nix
+++ hosts/mashnix/windows-entry.nix
@@ -5,4 +5,8 @@
       protocol: efi
       path: uuid(75ff7c8c-6e10-44eb-890b-e0160dfff8f0):/EFI/Microsoft/Boot/bootmgfw.efi
   '';
+
+  boot.loader.limine.extraConfig = ''
+    timeout: 60
+  '';
 }
